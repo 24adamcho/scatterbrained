e8f4deae56e05c872a78fa62846d6302f67b2e3e
Adam Cho <not@an.email>
2023-09-28 18:48:55 -0500

    changed classes to functional because react moment

--- src/Components/grapheditor/GraphEditor.js
+++ src/Components/grapheditor/GraphEditor.js
@@ -4,23 +4,17 @@ import './GraphEditor.css'
 import ReactFlow, { Controls, Background } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-class GraphEditor extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <>
-                <div style={{height:'100%'}}>
-                    <ReactFlow>
-                        <Background></Background>
-                        <Controls></Controls>
-                    </ReactFlow>
-                </div>
-            </>
-        )
-    }
+function GraphEditor(props){
+    return (
+        <>
+            <div style={{height:'100%'}}>
+                <ReactFlow>
+                    <Background></Background>
+                    <Controls></Controls>
+                </ReactFlow>
+            </div>
+        </>
+    )
 }
 
 export default GraphEditor;
--- src/Components/texteditor/TextEditor.js
+++ src/Components/texteditor/TextEditor.js
@@ -3,44 +3,37 @@ import ReactQuill from "react-quill";
 import './TextEditor.css'
 import 'react-quill/dist/quill.snow.css';
 
-class TextEditor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = props;
-
-        var toolbarOptions = [
+function TextEditor(props) {
+    const modules = {
+        toolbar: [
             [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
             [{ 'font': [] }],
             [{ 'size': [] }],
 
             ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
             ['blockquote', 'code-block'],
-          
+            
             [{ 'list': 'ordered'}, { 'list': 'bullet' }],
             [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
             [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
             [{ 'direction': 'rtl' }],                         // text direction
 
             ['link', 'image', 'video'],                       // rich media embed
-          
-          
+            
+            
             [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
             [{ 'align': [] }],
-          
+            
             ['clean']                                         // remove formatting button
-        ];
-        this.modules = {
-            toolbar: toolbarOptions
-        }
-    }
-
-    render() {
-        //const [value, setValue] = useState('');
-        return (
-            //<ReactQuill modules={this.modules} placeholder='Make a new note...' value={value} onChange={setValue} /> //useState don't work
-            <ReactQuill modules={this.modules} placeholder="Make a new note..." />
-        );
-    }
+        ]
+    };
+    
+    const [value, setValue] = useState('');
+    
+    return (
+        <ReactQuill modules={modules} placeholder='Make a new note...' value={value} onChange={setValue} /> //useState doesn't like classes
+        //<ReactQuill modules={modules} placeholder="Make a new note..." />
+    );
 }
 
 export default TextEditor;
