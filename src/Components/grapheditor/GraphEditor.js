02683a49a00d8ac78df2938d14f4578519278b51
Adam Cho <not@an.email>
2023-09-28 15:54:58 -0500

    init

---
+++ src/Components/grapheditor/GraphEditor.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './GraphEditor.css'
+
+
+class GraphEditor extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
