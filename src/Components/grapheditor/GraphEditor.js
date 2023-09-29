130a81b7edf5d7cb0f8f8e0bcf71c73812acbc3c
Adam Cho <not@an.email>
2023-09-28 19:00:41 -0500

    styling

--- src/Components/grapheditor/GraphEditor.js
+++ src/Components/grapheditor/GraphEditor.js
@@ -1,15 +1,17 @@
-import { Component } from 'react';
-import './GraphEditor.css'
+import { Component, useState} from 'react';
 
 import ReactFlow, { Controls, Background } from 'reactflow';
 import 'reactflow/dist/style.css';
+import './GraphEditor.css'
 
 function GraphEditor(props){
+    const [bgstyle, setBgstyle] = useState('cross');
+
     return (
         <>
             <div style={{height:'100%'}}>
                 <ReactFlow>
-                    <Background></Background>
+                    <Background variant={bgstyle}/>
                     <Controls></Controls>
                 </ReactFlow>
             </div>
