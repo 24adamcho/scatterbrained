3374ef2093014b64431002c2ce562b24cc6e1865
Adam Cho <not@an.email>
2023-09-28 22:32:00 -0500

    addNoteButton

--- src/Components/grapheditor/GraphEditor.js
+++ src/Components/grapheditor/GraphEditor.js
@@ -1,15 +1,21 @@
 import { Component, useState} from 'react';
 
 import ReactFlow, { Controls, Background } from 'reactflow';
+import { Button } from 'react-bootstrap';
+
 import 'reactflow/dist/style.css';
 import './GraphEditor.css'
+import {ReactComponent as AddNoteIcon } from './add-note-svgrepo-com.svg'
 
 function GraphEditor(props){
     const [bgstyle, setBgstyle] = useState('cross');
 
     return (
         <>
-            <div style={{height:'100%'}}>
+            <div className='flowInterfaceWrapper' style={{height:'100%'}}>
+                <Button className='addNoteButton' variant='primary'>
+                    <AddNoteIcon />
+                </Button>
                 <ReactFlow>
                     <Background variant={bgstyle}/>
                     <Controls></Controls>
