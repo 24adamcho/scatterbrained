dd088ed7ee2ff755b94c8aba9e1284e7a1fddb37
Adam <adam@adamcho.com>
2023-10-24 16:21:10 -0500

    styling

--- src/Components/grapheditor/TopbarContextMenu/Widgets/ColorPickerDropdown.js
+++ src/Components/grapheditor/TopbarContextMenu/Widgets/ColorPickerDropdown.js
@@ -1,19 +1,25 @@
-import { Dropdown } from "react-bootstrap";
+import { Button, Dropdown } from "react-bootstrap";
 import { RgbStringColorPicker, HexColorInput } from "react-colorful";
+
+import './ColorPickerDropdown.css'
 
 const ColorPickerDropdown = ({
     color,
-    onChange
+    onChange,
+    onReset
 })=>{
     return (
         <>
-            <Dropdown>
+            <Dropdown autoclose={true}>
                 <Dropdown.Toggle className="colorPickerButton">
                     Color picker
                 </Dropdown.Toggle>
                 <Dropdown.Menu className="colorPickerPopup">
                     <RgbStringColorPicker color={color} onChange={onChange}/>
-                    <HexColorInput color={color} onChange={onChange}/>
+                    <div className="colorPickerFields">
+                        <HexColorInput color={color} onChange={onChange}/>
+                        <Button onClick={onReset}>Reset</Button>
+                    </div>
                 </Dropdown.Menu>
             </Dropdown>
         </>
