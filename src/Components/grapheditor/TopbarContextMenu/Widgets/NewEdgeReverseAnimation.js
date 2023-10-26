7adf849410de5de6aa4ae81f76fc7624e1a92eae
Adam <adam@adamcho.com>
2023-10-26 13:59:38 -0500

    rename

---
+++ src/Components/grapheditor/TopbarContextMenu/Widgets/NewEdgeReverseAnimation.js
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+
+const NewEdgeReverseAnimation = ({
+    
+}) => {
+    const onClick = () => {
+        
+    }
+
+    return (
+        <Button onClick={()=>{onClick()}}>Reverse animation</Button>
+    )
+}
+
+export default NewEdgeReverseAnimation;
--- src/Components/grapheditor/TopbarContextMenu/Widgets/ReverseEdgeAnimation.js
+++
@@ -1,7 +0,0 @@
-const ReverseEdgeAnimation = ({
-
-}) => {
-
-}
-
-export default ReverseEdgeAnimation;
