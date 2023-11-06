317a33e5cd83c683df47325ead225adfee9d59ac
Adam <adam@adamcho.com>
2023-11-06 00:01:06 -0600

    text overflow gradient

--- src/Components/grapheditor/NoteNode.js
+++ src/Components/grapheditor/NoteNode.js
@@ -1,4 +1,4 @@
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import {Handle, Position, NodeResizer, NodeResizeControl, useStore} from 'reactflow';
 import ReactQuill from 'react-quill'; //for data display
 
@@ -20,11 +20,42 @@ const handleStyle = {
     }
 }
 
+const overflowStyle = {
+    fade: {
+        background: 'linear-gradient(transparent 75%, var(--color-mid)) 100%',
+    },
+    nofade: {}
+}
+
 const NoteNode = ({data, selected}) => {
     const connectionNodeId = useStore(connectionNodeIdSelector);
 
     const isConnecting = !!connectionNodeId;
     const isTarget = connectionNodeId && connectionNodeId !== data.id;
+
+    //apply linear gradient on bottom of node as a sign of overflowing text
+    const quillRef = useRef(null);
+    const [quillStyle, setQuillStyle] = useState(null);
+    const [height, setHeight] = useState(100)
+    const onResize = (event, params) => {
+        setHeight(params.height);
+    }
+    useEffect(()=>{
+        if(quillRef === undefined) return;
+        if(quillRef.current === undefined) return;
+        if(quillRef.current.editingArea === undefined) return;
+        if(quillRef.current.editingArea.firstChild === undefined) return;
+
+        const element = quillRef.current.editingArea.firstChild;
+        const isOverflowing = element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+        
+        if(isOverflowing) {
+            setQuillStyle(overflowStyle.fade);
+        }
+        else {
+            setQuillStyle(overflowStyle.nofade);
+        }
+    }, [height, quillRef, data.content])
 
     return (
         <>
@@ -44,6 +75,7 @@ const NoteNode = ({data, selected}) => {
                             height:'1.25rem', 
                             transform:'translate(-1.25rem, -1.25rem)'
                     }}
+                    onResize={onResize}
                 >
                     <ResizeHandle style={{width:'1.25rem', height:'1.25rem', transform:'translate(0px, -5px)'}}/>
                 </NodeResizeControl>
@@ -70,7 +102,8 @@ const NoteNode = ({data, selected}) => {
                 value={data.content}
                 placeholder='Empty note.'
                 readOnly={true}
-                // style={{padding:10}}
+                ref={quillRef}
+                style={quillStyle}
             />
         </>
     )
