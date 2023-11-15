e5eacf455195078f419fe0cfa666272479ece724
Adam <adam@adamcho.com>
2023-11-15 00:38:34 -0600

    hexcolorinput now works properly

--- src/Components/grapheditor/TopbarContextMenu/Widgets/ColorPickerDropdown.js
+++ src/Components/grapheditor/TopbarContextMenu/Widgets/ColorPickerDropdown.js
@@ -1,6 +1,8 @@
 import { Button, Dropdown } from "react-bootstrap";
 import { RgbStringColorPicker, HexColorInput } from "react-colorful";
 
+import { useEffect, useState } from "react";
+
 import './ColorPickerDropdown.css'
 
 const ColorPickerDropdown = ({
@@ -9,6 +11,45 @@ const ColorPickerDropdown = ({
     onReset,
     onToggle
 })=>{
+    const [hexColor, setHexColor] = useState('#000000')    
+
+    //a bunch of these are magic bullshit functions
+    //see FelipeC's answer to https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    function rgbToHex(r, g, b) {
+        if(r === undefined || r === null) return '#000000';
+        if(g === undefined || g === null) return '#000000';
+        if(b === undefined || b === null) return '#000000';
+
+        const rgb = (r << 16) | (g << 8) | (b << 0);
+        if(rgb === undefined || rgb === null) return '#000000';
+
+        return '#' + (0x1000000 + rgb).toString(16).slice(1);
+    }
+
+    function hexToRgb(hex) {      
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        
+        return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : null;
+    }
+
+    //and RajeshP's answer to https://stackoverflow.com/questions/42827884/split-a-number-from-a-string-in-javascript
+    //this function is to assign hexColorInput's text after a change, converting rgb(r,g,b) to hex
+    useEffect(()=>{
+        var s = color.match(/\d+/g);
+        setHexColor(rgbToHex(s[0], s[1], s[2]))
+    },[color])
+
+    //and this one to change color to the new hex input, converted to rgb(r,g,b) format
+    function hexChangeColor(hex) {
+        var rgb = hexToRgb(hex);
+        var str = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`
+        onChange(str);
+    }
+    
     return (
         <>
             <Dropdown autoclose="true" onToggle={onToggle}>
@@ -37,7 +78,7 @@ const ColorPickerDropdown = ({
                     </span>
                     <RgbStringColorPicker color={color} onChange={onChange}/>
                     <div className="colorPickerFields">
-                        <HexColorInput color={color} onChange={onChange}/>
+                        <HexColorInput color={hexColor} onChange={hexChangeColor} prefixed={true}/>
                         <Button onClick={onReset}>Reset</Button>
                     </div>
                 </Dropdown.Menu>
